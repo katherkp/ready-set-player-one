0cf0e990171bae05ff7238922e0af4892d9a47e6
Joshua <joshua.lopez8893@yahoo.com>
2022-12-06 19:35:14 -0800

    got app working again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -15,7 +16,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
